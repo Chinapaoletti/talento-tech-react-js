e4bc84e9b59d20acee047f4eb015fd85b81dcbce
CHINA\chinapaoletti <chinapaoletti@gmail.com>
2026-07-13 09:26:47 -0300

    Move Firebase config to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,15 @@
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB8s8JuOlx6sGD8PgHE_xgZO0AwCoXDaIM",
-  authDomain: "talentotech-reactjs-01.firebaseapp.com",
-  projectId: "talentotech-reactjs-01",
-  storageBucket: "talentotech-reactjs-01.firebasestorage.app",
-  messagingSenderId: "52916981697",
-  appId: "1:52916981697:web:3318adac4d1295fbeae940"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
